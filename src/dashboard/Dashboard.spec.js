96326f2edec57b6389e728baa16958bcd773d68f
Kronykus <MoreheadSteve@gmail.com>
2019-11-15 09:51:38 -0600

    late but done

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test( 'Dashboard renders correctly', () => {
+    expect( render( <Dashboard /> ) ).toMatchSnapshot();
+} );
+
+test( 'is displaying controls component', () => {
+    const { queryByTestId } = render( <Dashboard /> );
+
+    const element = queryByTestId( 'controlsTest' );
+    expect( element ).toBeTruthy();
+} );
+
+test( 'is displaying display component', () => {
+    const { queryByTestId } = render( <Dashboard /> );
+
+    const element = queryByTestId( 'displayTest' );
+    expect( element ).toBeTruthy();
+} );
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,68 @@
-// Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import Display from './Display';
+
+test( 'Display renders correctly', () => {
+    expect( render( <Display /> ) ).toMatchSnapshot();
+} );
+
+
+test( 'displays green led class', () => {
+    const { getByText } = render(
+        <Display locked={ false } closed={ false } />
+    );
+
+    const unlocked = getByText( /unlocked/i );
+    expect( unlocked.className ).toMatch( "green-led" );
+
+    const open = getByText( /open/i );
+    expect( open.className ).toMatch( "green-led" );
+} );
+
+test( 'displays red led class', () => {
+    const { getByText } = render(
+        <Display locked={ true } closed={ true } />
+    );
+
+    const locked = getByText( /locked/i );
+    expect( locked.className ).toMatch( "red-led" );
+
+    const closed = getByText( /closed/i );
+    expect( closed.className ).toMatch( "red-led" );
+} );
+
+test( 'displays unlocked and closed text', () => {
+    const { findByText } = render(
+        <Display locked={ false } open={ false } />
+    );
+
+    const unlockedMessage = findByText( /unlocked/i );
+    expect( unlockedMessage ).toBeTruthy();
+
+    const closedMessage = findByText( /closed/i );
+    expect( closedMessage ).toBeTruthy();
+} );
+
+test( 'displays unlocked and open text', () => {
+    const { findByText } = render(
+        <Display locked={ false } open={ true } />
+    );
+
+    const unlockedMessage = findByText( /unlocked/i );
+    expect( unlockedMessage ).toBeTruthy();
+
+    const openMessage = findByText( /open/i );
+    expect( openMessage ).toBeTruthy();
+} );
+
+test( 'displays locked and closed text', () => {
+    const { findByText } = render(
+        <Display locked={ true } open={ false } />
+    );
+
+    const lockedMessage = findByText( /locked/i );
+    expect( lockedMessage ).toBeTruthy();
+
+    const closedMessage = findByText( /closed/i );
+    expect( closedMessage ).toBeTruthy();
+} );
